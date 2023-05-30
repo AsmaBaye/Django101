8c7f9e9549b18dc3e8d82a46ceeb4092270f4b73
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-05-30 14:22:30 +0300

    update tasks/views

--- myproject/tasks/views.py
+++ myproject/tasks/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 mytasklist = ['5AM', '30X', 'RJS and VSC']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = "New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 def taskList(request):
     return render(request, 'tasks/lists.html', 
                {"tasks": mytasklist})
  
 def addTask(request):
- 	return render(request, 'tasks/add.html')
+ 	return render(request, 'tasks/add.html',
+                {"form": NewTaskForm()})
 
 def index(request):
 	return render(request, 'tasks/index.html')
