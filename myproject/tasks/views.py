38967c7110cf936feed46a958d6f78b57d5a31d7
AsmaBaye <asmamaw.lejalem7@gmail.com>
2023-05-30 13:57:25 +0300

    update files

--- myproject/tasks/views.py
+++ myproject/tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+mytasklist = ['5AM', '30X', 'RJS and VSC']
+def taskList(request):
+    return render(request, 'tasks/lists.html', 
+               {"tasks": mytasklist})
+ 
+def addTask(request):
+ 	return render(request, 'tasks/add.html')
+
+def index(request):
+	return render(request, 'tasks/index.html')
